00462aa805c0dc3051e915d4238d2ab363cb7906
NK8998 <ne.kioko16@gmail.com>
2024-01-04 01:41:35 +0300

    dadd

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.options("*", cors());
+
+app.use(
+  cors({
+    origin: "*",
+    // credentials: true,
+  })
+);
+
+app.use(express.json());
+const multer = require("multer");
+const generateMPDandUpload = require("./transcoder");
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post(
+  "/transcode",
+  upload.fields([{ name: "video", maxCount: 1 }]),
+  generateMPDandUpload
+);
+
+app.listen("5573", () => {
+  console.log("listening on port 5573");
+});
