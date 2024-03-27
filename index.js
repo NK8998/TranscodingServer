593db53faad90850d3118c35fbd0d7143785b25d
NK8998 <ne.kioko16@gmail.com>
2024-03-27 22:54:41 +0300

    asn

--- index.js
+++ index.js
@@ -2,26 +2,38 @@ const { createClient } = require("@supabase/supabase-js");
 const { updateInternalQueue, startJobs } = require("./endpoints/functions/queueController");
 require("dotenv").config();
 
+const maxStartupDelay = 1000;
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-const channel = supabase
-  .channel("video-queue")
-  .on(
-    "postgres_changes",
-    {
-      event: "INSERT",
-      schema: "public",
-    },
-    (payload) => {
-      console.log("guess what I fucking ran anyway!!!");
-      // setUpTranscodingJobs();
-      console.log(payload.new);
-      updateInternalQueue(payload.new);
-    }
-  )
-  .subscribe();
+const subscribeToSupabase = () => {
+  const randomDelay = Math.floor(Math.random() * maxStartupDelay);
+  console.log(randomDelay);
+  setTimeout(() => {
+    const channel = supabase
+      .channel("video-queue")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+        },
+        (payload) => {
+          console.log("guess what I fucking ran anyway!!!");
+          // setUpTranscodingJobs();
+          console.log(payload.new);
+          updateInternalQueue(payload.new);
+        }
+      )
+      .subscribe();
+  }, randomDelay);
+};
 
-startJobs();
+async function startInstaceJob() {
+  subscribeToSupabase();
+  startJobs();
+}
+
+startInstaceJob();
 
 // git clone https://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@github.com/NK8998/TranscodingServer
 
