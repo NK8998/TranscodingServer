229c159e989ad7319b360ba5053cb26238261dcc
NK8998 <ne.kioko16@gmail.com>
2024-03-27 16:08:33 +0300

    asna

--- endpoints/functions/getInstanceId.js
+++ endpoints/functions/getInstanceId.js
@@ -4,7 +4,8 @@ let instanceId;
 const getInstanceId = () => {
   return new Promise((resolve, reject) => {
     const command =
-      'curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id';
+      'TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id';
     exec(command, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
