63527564ae42ad91a3391691667d0835815d8eab
NK8998 <ne.kioko16@gmail.com>
2024-03-27 16:04:39 +0300

    asns

--- endpoints/functions/getInstanceId.js
+++ endpoints/functions/getInstanceId.js
@@ -1,31 +1,19 @@
-const http = require("http");
+const { exec } = require("child_process");
 let instanceId;
 
 const getInstanceId = () => {
   return new Promise((resolve, reject) => {
-    const options = {
-      hostname: "169.254.169.254",
-      port: 80,
-      path: "/latest/meta-data/instance-id",
-      method: "GET",
-    };
-
-    const req = http.request(options, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-      res.on("end", () => {
-        resolve(data);
-        instanceId = data;
-      });
+    const command =
+      'curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600" && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id';
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(error);
+      } else {
+        resolve(stdout.trim());
+        instanceId = stdout.trim();
+      }
     });
-
-    req.on("error", (error) => {
-      reject(`Problem with request: ${error.message}`);
-    });
-
-    req.end();
   });
 };
 
