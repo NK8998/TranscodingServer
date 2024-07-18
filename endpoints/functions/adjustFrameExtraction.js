ebc0ba08ffc5387f7cba4bb5b2695bd4936319f9
NK8998 <ne.kioko16@gmail.com>
2024-07-18 10:20:32 +0300

    sdnsd

--- endpoints/functions/adjustFrameExtraction.js
+++ endpoints/functions/adjustFrameExtraction.js
@@ -7,19 +7,19 @@ const adjustFrameExtraction = (duration) => {
     paletteSize = 3; // set palette size to 3
   } else if (duration <= 300) {
     // for videos of 1-5 minutes
-    extractionRate = 1; // extract a frame every 1 seconds
-    paletteSize = 4; // set palette size to 3
+    extractionRate = 2; // extract a frame every 2 seconds
+    paletteSize = 4; // set palette size to 4
   } else if (duration <= 1800) {
     // for videos of 5-30 minutes
-    extractionRate = 3;
+    extractionRate = 5;
     paletteSize = 4;
   } else if (duration <= 3600) {
     // for videos of 30 minutes to 1 hour
-    extractionRate = 4; // extract a frame every 5 seconds
+    extractionRate = 10; // extract a frame every 10 seconds
     paletteSize = 4; // set palette size to 4
   } else {
     // for videos longer than 1 hour
-    extractionRate = 5; // extract a frame every 5 seconds
+    extractionRate = 10; // extract a frame every 10 seconds
     paletteSize = 4; // set palette size to 4
   }
 
